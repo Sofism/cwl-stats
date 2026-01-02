d2b5f9d29f2ee189cf3c763af7b7bbf5876f5810
Sofism <sbatalla1@hotmail.com>
2026-01-02 15:39:59 +0100

    Create cosntants.js

---
+++ utils/cosntants.js
@@ -0,0 +1,36 @@
+export const LEAGUES = [
+  "Champion I",
+  "Champion II",
+  "Champion III",
+  "Master I",
+  "Master II",
+  "Master III",
+  "Crystal I",
+  "Crystal II",
+  "Crystal III",
+];
+
+export const BONUSES = {
+  "Champion I": [10, 9, 8, 7, 6, 5, 4, 3],
+  "Champion II": [10, 9, 8, 7, 6, 5, 4, 3],
+  "Champion III": [10, 9, 8, 7, 6, 5, 4, 3],
+  "Master I": [10, 9, 8, 7, 6, 5, 4, 3],
+  "Master II": [10, 9, 8, 7, 6, 5, 4, 3],
+  "Master III": [10, 9, 8, 7, 6, 5, 4, 3],
+  "Crystal I": [9, 8, 7, 6, 5, 4, 3, 2],
+  "Crystal II": [9, 8, 7, 6, 5, 4, 3, 2],
+  "Crystal III": [9, 8, 7, 6, 5, 4, 3, 2],
+};
+
+export const DEFAULT_VISIBLE_COLS = {
+  th: true,
+  missAtk: true,
+  missDef: false,
+  netStars: true,
+  netPercent: false,
+  threeRate: true,
+  starGain: false,
+  percentGain: false,
+  starGive: false,
+  percentGive: false,
+};
